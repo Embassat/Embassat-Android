600bdf6b85d7202aa67b706c3e22b1d956cc1021
Enrique Álvarez Monedero <Quique@ip-192-168-10-128.eu-west-1.compute.internal>
2016-06-01 14:51:11 +0200

    Add playlist to menu

--- app/src/main/kotlin/com/embassat/screen/MainActivity.kt
+++ app/src/main/kotlin/com/embassat/screen/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.embassat.screen
 
+import android.content.Intent
+import android.net.Uri
 import android.widget.TextView
 import com.embassat.R
 import com.embassat.base.BaseActivity
 import com.embassat.extension.navigate
 
-public class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity() {
 
     override val layoutResource: Int = R.layout.activity_main
 
@@ -16,6 +18,7 @@ public class MainActivity : BaseActivity() {
     lateinit var mapTickets : TextView
     lateinit var mapExtra : TextView
     lateinit var transportExtra : TextView
+    lateinit var playlist : TextView
 
     override fun init() {
         optionInfo = findViewById(R.id.main_option_info) as TextView
@@ -25,6 +28,7 @@ public class MainActivity : BaseActivity() {
         mapTickets = findViewById(R.id.main_option_tickets) as TextView
         mapExtra = findViewById(R.id.main_option_extra) as TextView
         transportExtra = findViewById(R.id.main_option_transport) as TextView
+        playlist = findViewById(R.id.main_option_playlist) as TextView
         optionInfo.setOnClickListener { navigate<InfoActivity>() }
         optionArtists.setOnClickListener { navigate<ArtistsActivity>() }
         optionSchedule.setOnClickListener { navigate<ScheduleActivity>() }
@@ -32,5 +36,10 @@ public class MainActivity : BaseActivity() {
         mapTickets.setOnClickListener { navigate<TicketsActivity>() }
         mapExtra.setOnClickListener { navigate<ExtraActivity>() }
         transportExtra.setOnClickListener { navigate<TransportActivity>() }
+        playlist.setOnClickListener {
+            val uri = Uri.parse("spotify:user:embassat:playlist:16SGgn6bS6uQnImxWzZfrc")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 }
